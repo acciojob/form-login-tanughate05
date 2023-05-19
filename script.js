9a97078bac5b1513b9746a19471d6f7f019cab0f
tanughate05 <93458822+tanughate05@users.noreply.github.com>
2023-05-19 11:37:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+const  firstname=document.getElementById("form1").elements["fname"].value;
+	const  lastname= document.getElementById("form1").elements["lname"].value;
 
+	const fullname = firstname + " " + lastname;
+	alert(fullname);
 }
